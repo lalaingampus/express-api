aa2815ab36cf6f39874eccb45dec77a3d322219e
devmccoy <andrian@mccoy.com.sg>
2025-11-19 14:22:03 +0700

    fix: perbaikan cron

--- src/models/cronlog.js
+++ src/models/cronlog.js
@@ -1,26 +1,35 @@
 // src/models/CronLog.js
-
 const { DataTypes } = require("sequelize");
-const sequelize = require("../../config/database");
 
-const CronLog = sequelize.define("CronLog", {
-  type: {
-    type: DataTypes.STRING,
-    allowNull: false, // daily, weekly, monthly
-  },
-  status: {
-    type: DataTypes.STRING,
-    defaultValue: "success", // success / failed
-  },
-  message: {
-    type: DataTypes.TEXT,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-  }
-}, {
-  tableName: "cron_logs",
-});
+module.exports = (sequelize) => {
+  const CronLog = sequelize.define(
+    "CronLog",
+    {
+      type: {
+        type: DataTypes.ENUM("daily", "weekly", "monthly"),
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM("success", "failed"),
+        allowNull: false,
+        defaultValue: "success",
+      },
+      message: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        field: "user_id",
+      },
+    },
+    {
+      tableName: "cron_logs",
+      timestamps: true,
+      underscored: true, // optional kalau kamu pakai snake_case
+    }
+  );
 
-module.exports = CronLog;
+  return CronLog;
+};
